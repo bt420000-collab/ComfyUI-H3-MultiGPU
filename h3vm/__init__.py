5de79886090390f6e9491b4e15ae007fbe04df45
贝丝小草 <bt420000@gmail.com>
2026-08-22 01:47:06 +0800

    Install shared Core bridge on runtime package import

--- h3vm/__init__.py
+++ h3vm/__init__.py
@@ -1,1 +1,6 @@
 """H3VM runtime package. Heavy CUDA/Comfy imports remain lazy."""
+
+from .core_adapter import install_runtime_bridge as _install_runtime_bridge
+
+_install_runtime_bridge()
+del _install_runtime_bridge
